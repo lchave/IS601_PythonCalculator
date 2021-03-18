4a81f585d4dbc4578946d768b780ee159dba5f50
Luis <61847499+lchave@users.noreply.github.com>
2021-03-17 22:27:26 -0400

    Feature: Added subtraction method to calculator and the corresponding test.

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -12,5 +12,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(calculator.add(1,2),3)
         self.assertEqual(calculator.result,3)
 
+    def test_subtraction_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2,1),1)
+        self.assertEqual(calculator.result,1)
+
 if __name__ == '__main__':
     unittest.main()
