30fbc503a0496daca389a797bf7e822557bc7bbe
Luis <61847499+lchave@users.noreply.github.com>
2021-03-17 22:16:57 -0400

    Task: Added addition method to calculator and the corresponding test.

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,10 +2,15 @@ import unittest
 from Calculator import  Calculator
 
 class MyTestCase(unittest.TestCase):
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_addition_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1,2),3)
+        self.assertEqual(calculator.result,3)
 
 if __name__ == '__main__':
     unittest.main()
